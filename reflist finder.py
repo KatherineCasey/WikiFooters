7f8dee2ffa0016f0b632023d957d4c60cc476929
Katherine Casey <fluffernutter.wiki@gmail.com>
2013-05-21 12:33:22 -0400

    fix URL bug, add external links

--- reflist finder.py
+++ reflist finder.py
@@ -10,35 +10,53 @@ def wikitextGrabber(pageName):
     rawtext = urlopen(URL)
     text = str(rawtext.readline())
     text = text.split('\\n')
-    #print (text)
     return text
+    #returns a list of the lines in the text, split at newline
 
-def findReflist(pageName):
-    text =  wikitextGrabber(pageName)
+def findReflist(text, pageName):
+    #text =  wikitextGrabber(pageName)
     #print (text)
     for line in text:
         #print (line)
         matchObjTemplate = re.search(r'\{{2}[Rr]eflist', line)
         matchObjTag = re.search(r'\<[Rr]eferences', line)
         if matchObjTemplate or matchObjTag:
-           print (line)
+           print ("References found in",pageName)
            return True
         else:
            next
     else:
+         print ("No references found in", pageName)
          return False
 
-def findExternalLinks(pageName):
-    pass
+def findExternalLinks(text):
+    extLinkLoc = 'none'
+    linkIden = re.compile(r'[eE]xternal [Ll]inks')
+    for index in range(0,len(text)-1):
+        matchLinks = re.search(linkIden, text[index])
+        if matchLinks:
+           extLinkLoc = index
+           match = True
+           return [match, extLinkLoc]
+        else:
+           next
+    else:
+         match = False
+         return [match, extLinkLoc]  
 
 f = open("articles list.txt", 'r')
 #files = f.read()
 for line in f:
     line = str(line)
     #print (line)
-    refFound = findReflist(line)
-    if refFound == True:
-       print ("References found in",line)
+    text = wikitextGrabber(line)
+    if findReflist(text, line):
+       print (line)
+       matchFound = findExternalLinks(text)[0]
+       print (matchFound)
+       matchLoc = findExternalLinks(text)[1]
+       print (matchLoc)
+       print ('---------')
     else:
-       print ("No references found in",line)
+       next
 f.close()
